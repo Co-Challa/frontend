57843cef2fd87199d8724c975fc8c589f325f047
surnavy <113087821+surnavy@users.noreply.github.com>
2025-05-18 21:59:05 +0900

    Update LoginPage.jsx
    feat. 로그인 기능 추가

--- src/pages/LoginPage.jsx
+++ src/pages/LoginPage.jsx
@@ -1,3 +1,45 @@
+import axios from "axios";
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
+
 export default function LoginPage() {
-  return <>Login Page</>;
+
+  const navigate = useNavigate();
+
+  const [form, setForm] = useState({
+    userId: "",
+    password: "",
+  });
+
+  const handleChange = (e) => {
+    setForm({...form, [e.target.name]: e.target.value});
+  }
+  
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    try{
+      const response = await axios.post("http://localhost:8080/signin", form);
+      const token = response.headers["authorization"]?.split(" ")[1];
+      console.log(response)
+
+      if (token) {
+        localStorage.setItem("token", token);
+        alert("로그인 성공!");
+        navigate("/");
+      } else {
+        alert("토큰이 없습니다.");
+      }
+    } catch (err) {
+       const msg = err.response?.data ?? "로그인 실패";
+        alert("에러: " + msg);
+    }
+  }
+  return (
+    <form onSubmit={handleSubmit}>
+      <h2>로그인</h2>
+      <input name="userId" placeholder="유저아이디" value={form.userId} onChange={handleChange} required/>
+      <input name="password" type="password" placeholder="비밀번호" value={form.password} onChange={handleChange} required/>
+      <button type="submit">로그인</button>
+    </form>
+  );
 }
