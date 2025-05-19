3b4c6c849e08dca5cd3cc4e86d2224ab0703f2a9
seoyun <chaeseoyoun8694@gmail.com>
2025-05-19 14:54:15 +0900

    feat. 무한스크롤 기능 및 MyPage의 내 게시글 섹션 적용 #3

--- src/pages/MyPage.jsx
+++ src/pages/MyPage.jsx
@@ -1,12 +1,24 @@
 import React, { useState } from 'react';
+import './Mypage.css'
 import useUserInfo from '../hooks/useUserInfo';
 import MyPageHeader from '../components/MypageHeader';
 import MyPageNav from '../components/MyPageNav';
+
+import { fetchUserPosts } from '../apis/userApi';
+import useInfiniteList from '../hooks/useInfiniteList';
+import UserPostCard from '../components/UserPostCard';
 
 export default function MyPage() {
   const { user, loading, error } = useUserInfo();
   const [activeTab, setActiveTab] = useState('내 게시글');
   const tabs = ['내 게시글', '관심 게시글', '내 댓글'];
+
+  const {
+    items: posts,
+    hasMore: postsHasMore,
+    loading: postsLoading,
+    lastRef: postsLastRef
+  } = useInfiniteList(fetchUserPosts, 10);
 
   if (loading) return <div>로딩 중...</div>;
   if (error)   return <div>오류 발생</div>;
@@ -22,7 +34,22 @@ export default function MyPage() {
       />
 
       <section>
-        {activeTab === '내 게시글' && <div>내 게시글 리스트</div>}
+        {activeTab === '내 게시글' && (
+          <div className="userpost_list">
+            {posts.map((post, i) =>
+              i === posts.length - 1 ? (
+                <div key={post.post_id} ref={postsLastRef}>
+                  <UserPostCard post={post} />
+                </div>
+              ) : (
+                <UserPostCard key={post.post_id} post={post} />
+              )
+            )}
+            {postsLoading && <div className="loading">로딩 중...</div>}
+            {!postsHasMore && <div className="end">모든 게시물을 불러왔습니다</div>}
+          </div>
+        )}
+
         {activeTab === '관심 게시글' && <div>관심 게시글 리스트</div>}
         {activeTab === '내 댓글' && <div>내 댓글 리스트</div>}
       </section>
