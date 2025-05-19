799923783c506f0a2f2f7071e63a86fadcb48739
seoyun <chaeseoyoun8694@gmail.com>
2025-05-19 13:33:37 +0900

    feat. Mypage 섹션별 내용 GET API #3

--- src/apis/userApi.js
+++ src/apis/userApi.js
@@ -9,3 +9,39 @@ export async function fetchUserById() {
     const { data } = await axiosInstance.get(`/user/${userId}`);
     return data;
 }
+
+export async function fetchUserPosts(offset, limit) {
+    const userId = localStorage.getItem('user_id');
+    if (!userId) {
+        throw new Error('user_id가 로컬스토리지에 없습니다.');
+    }
+
+    const { data } = await axiosInstance.get(`/user/list/${userId}`, {
+        params: { offset, limit },
+    });
+    return data;
+}
+
+export async function fetchUserLiked(offset, limit) {
+    const userId = localStorage.getItem('user_id');
+    if (!userId) {
+        throw new Error('user_id가 로컬스토리지에 없습니다.');
+    }
+
+    const { data } = await axiosInstance.get(`/user/liked/${userId}`, {
+        params: { offset, limit },
+    });
+    return data;
+}
+
+export async function fetchUserComments(offset, limit) {
+    const userId = localStorage.getItem('user_id');
+    if (!userId) {
+        throw new Error('user_id가 로컬스토리지에 없습니다.');
+    }
+
+    const { data } = await axiosInstance.get(`/user/comment/${userId}`, {
+        params: { offset, limit },
+    });
+    return data;
+}
