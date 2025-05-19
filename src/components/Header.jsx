828c787810a0daf18781468cb38964d6f0fe4fb6
NamSangwon <127469500+NamSangwon@users.noreply.github.com>
2025-05-19 18:17:14 +0900

    feat. 헤더 스타일 추가

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,5 +1,41 @@
 import "./header.css";
 
 export default function Header() {
-  return <nav>header</nav>;
+	return (
+		<>
+			<header className="header_wrapper">
+				<div className="header_frame" />
+
+				<div className="header_frame_shadow" />
+
+				<div class="header_content_frame">
+					<div className="cochalla_frame">
+						<img class="cochalla_logo" src="src\assets\logo\logo.png" />
+						<span class="cochalla_text">Cochalla</span>
+					</div>
+
+					<div className="feature_frame">
+						<div className="question_box">
+							<span className="question_text">질문하기</span>
+							<img className="question_icon" src="src\assets\icons\stars.png"/>
+						</div>
+						<div>
+							<span className="login">로그인</span>
+						</div>
+					</div>
+
+					{/* <div className="feature_frame">
+						<div className="question_box">
+							<span className="question_text">질문하기</span>
+							<img className="question_icon" src="src\assets\icons\stars.png"/>
+						</div>
+						<div>
+							<img className="user_img" src="src\assets\images\profile\profile_1.png" />
+						</div>
+					</div> */}
+				</div>
+
+			</header>
+		</>
+	);
 }
