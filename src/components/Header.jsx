3daf517b2294ab679ac6635faa967d95131b6ddf
NamSangwon <127469500+NamSangwon@users.noreply.github.com>
2025-05-20 22:44:42 +0900

    feat. 로그인 여부에 따른 출력 변경 (로그인 or 유저 아이콘)

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,6 +2,32 @@ import { Link } from "react-router-dom";
 import "./header.css";
 
 export default function Header() {
+
+  const checkLoggedIn = () => { 
+    const token = localStorage.getItem('authToken');
+
+    if (!token) return false;
+
+    try {
+      const decodedToken = jwtDecode(token);
+      const currentTime = Math.floor(Date.now() / 1000); // 현재 시간을 초 단위로
+
+      // 토큰의 만료 시간 확인
+      if (decodedToken.exp && decodedToken.exp > currentTime) {
+        return true;
+      }
+      // 토큰이 만료되었으면 삭제 
+      else {
+        clearAuthToken();
+        return false;
+      }
+    } catch (error) {
+      console.error('Decoding Error : ', error);
+      clearAuthToken();
+      return false;
+    }
+  };
+
   return (
     <>
       <header className="header_wrapper">
@@ -24,16 +50,21 @@ export default function Header() {
                 <img className="question_icon" src="src\assets\icons\stars.png" />
               </div>
             </Link>
-            <Link to="/login">
-              <div>
-                <span className="login">로그인</span>
-              </div>
-            </Link>
-            <Link to="/mypage">
-              <div>
-                <img className="user_img" src="src\assets\images\profile\profile_1.png" />
-              </div>
-            </Link>
+            {
+              checkLoggedIn() ? (
+                <Link to="/mypage">
+                  <div>
+                    <img className="user_img" src="src\assets\images\profile\profile_1.png" />
+                  </div>
+                </Link>
+              ) : (
+                <Link to="/login">
+                  <div>
+                    <span className="login">로그인</span>
+                  </div>
+                </Link>
+              )
+            }
           </div>
         </div>
 
