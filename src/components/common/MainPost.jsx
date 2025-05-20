1cb9c6a1ce3d1d8db9567f16283278aa2003dc4c
dahee <ldhdh1832@gmail.com>
2025-05-21 00:02:56 +0900

    api연결

--- src/components/common/MainPost.jsx
+++ src/components/common/MainPost.jsx
@@ -7,23 +7,23 @@ import ko from "date-fns/locale/ko";
 export default function MainPost({ post }) {
      if (!post) return <div>게시글 정보 없음</div>;
 
-    const { profileImg, nickname, title, created_At, content, likes_count,  comments_count} = post;
+    const { userImage, nickname, title, createdAt, content, likesCount,  commentsCount} = post;
     const summary = content.split("\n").slice(0, 5).join("\n");
 
     return (
     <div className="summary_post">
         <div className="post_title">{title}</div>
       {/* 프로필 */}
-      <div className="flex items-center space-x-4 mb-2">
+      <div className="profile">
         <img
-          src={profileImg}
+          src={userImage}
           alt="사용자 이미지"
           className="user_image"
         />
         <div>
           <div className="user_nickname">post by{nickname}</div>
           <div className="created_At">
-            {formatDistanceToNow(new Date(created_At), {
+            {formatDistanceToNow(new Date(createdAt), {
               addSuffix: true,
               locale: ko,
             })}
@@ -35,13 +35,14 @@ export default function MainPost({ post }) {
       <pre className="post_summary">{summary}</pre>
 
       {/* 좋아요와 댓글 */}
-      <div className="likes_comments">
+      <div className="count_icon">
         <div className="likes">
-            <img src="../assets/icons/heart.png" alt="하트아이콘" />
-          <span>{likes_count}</span>
+            {/* 아이콘 이렇게 넣는건지 모르겠네;; */}
+            <img src="../assets/icons/heart.png" alt="하트아이콘" /> 
+          <span>{likesCount}</span>
         </div>
         <div className="comments">
-          <span>{comments_count}</span>
+          <span>{commentsCount}</span>
         </div>
       </div>
     </div>
