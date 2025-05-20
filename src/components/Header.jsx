50057684f48d5f0628a30fdf22c3d57daae0d256
NamSangwon <127469500+NamSangwon@users.noreply.github.com>
2025-05-20 13:36:30 +0900

    feat. 로고, 질문, 로그인 및 마이페이지 Link 기능 추가

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,41 +1,43 @@
+import { Link } from "react-router-dom";
 import "./header.css";
 
 export default function Header() {
-	return (
-		<>
-			<header className="header_wrapper">
-				<div className="header_frame" />
+  return (
+    <>
+      <header className="header_wrapper">
+        <div className="header_frame" />
 
-				<div className="header_frame_shadow" />
+        <div className="header_frame_shadow" />
 
-				<div class="header_content_frame">
-					<div className="cochalla_frame">
-						<img class="cochalla_logo" src="src\assets\logo\logo.png" />
-						<span class="cochalla_text">Cochalla</span>
-					</div>
+        <div className="header_content_frame">
+          <Link to="/">
+            <div className="cochalla_frame">
+              <img className="cochalla_logo" src="src\assets\logo\logo.png" />
+              <span className="cochalla_text">Cochalla</span>
+            </div>
+          </Link>
 
-					<div className="feature_frame">
-						<div className="question_box">
-							<span className="question_text">질문하기</span>
-							<img className="question_icon" src="src\assets\icons\stars.png"/>
-						</div>
-						<div>
-							<span className="login">로그인</span>
-						</div>
-					</div>
+          <div className="feature_frame">
+            <Link to="/chat">
+              <div className="question_box">
+                <span className="question_text">질문하기</span>
+                <img className="question_icon" src="src\assets\icons\stars.png" />
+              </div>
+            </Link>
+            <Link to="/login">
+              <div>
+                <span className="login">로그인</span>
+              </div>
+            </Link>
+            <Link to="/mypage">
+              <div>
+                <img className="user_img" src="src\assets\images\profile\profile_1.png" />
+              </div>
+            </Link>
+          </div>
+        </div>
 
-					{/* <div className="feature_frame">
-						<div className="question_box">
-							<span className="question_text">질문하기</span>
-							<img className="question_icon" src="src\assets\icons\stars.png"/>
-						</div>
-						<div>
-							<img className="user_img" src="src\assets\images\profile\profile_1.png" />
-						</div>
-					</div> */}
-				</div>
-
-			</header>
-		</>
-	);
+      </header>
+    </>
+  );
 }
