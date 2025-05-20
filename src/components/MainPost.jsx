a4dc3fe16970bee556b1e45af3f16e69832f6ebe
dahee <ldhdh1832@gmail.com>
2025-05-21 02:59:02 +0900

    add post css

--- src/components/MainPost.jsx
+++ src/components/MainPost.jsx
@@ -5,23 +5,18 @@ import { formatDistanceToNow } from "date-fns";
 import ko from "date-fns/locale/ko";
 
 export default function MainPost({ post }) {
-     if (!post) return <div>게시글 정보 없음</div>;
+  if (!post) return <div>게시글 정보 없음</div>;
 
-    const { userImage, nickname, title, createdAt, content, likesCount,  commentsCount} = post;
-    const summary = content.split("\n").slice(0, 5).join("\n");
+  const { userImage, nickname, title, createdAt, content, likesCount, commentsCount } = post;
+  const summary = content.split("\n").slice(0, 5).join("\n");
 
-    return (
+  return (
     <div className="summary_post">
-        <div className="post_title">{title}</div>
       {/* 프로필 */}
       <div className="profile">
-        <img
-          src={userImage}
-          alt="사용자 이미지"
-          className="user_image"
-        />
-        <div>
-         <div className="user_nickname">
+        <img src={userImage} alt="사용자 이미지" className="user_image" />
+        <div className="nickname_time">
+          <div className="user_nickname">
             <span className="prefix">post by </span>
             <span className="nickname">{nickname}</span>
           </div>
@@ -34,20 +29,23 @@ export default function MainPost({ post }) {
         </div>
       </div>
 
+      <div className="post_title">{title}</div>
+
       {/* 게시글 요약 */}
-      <pre className="post_summary">{summary}</pre>
+      <div className="post_summary">{summary}</div>
 
       {/* 좋아요와 댓글 */}
       <div className="count_icon">
-        <div className="likes">
-            {/* 아이콘 이렇게 넣는건지 모르겠네;; */}
-            <img src="../assets/icons/heart.png" alt="하트아이콘" /> 
+        <div className="icon_group">
+          <div className="likes"></div>
           <span>{likesCount}</span>
         </div>
-        <div className="comments">
+        <div className="icon_group">
+          <div className="comments"></div>
           <span>{commentsCount}</span>
         </div>
       </div>
+
     </div>
   );
 }
