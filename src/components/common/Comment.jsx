05842c11e5b50fa982ea91b17a65b9208a443366
NamSangwon <127469500+NamSangwon@users.noreply.github.com>
2025-05-21 16:55:34 +0900

    fix. API 및 유틸 함수 분리 & 무한스크롤 훅 함수 적용

--- src/components/common/Comment.jsx
+++ src/components/common/Comment.jsx
@@ -1,122 +1,63 @@
-import React, { useState, useEffect, useRef } from 'react';
-import { jwtDecode } from 'jwt-decode';
+import React, { useCallback } from 'react';
 
-import axiosInstance from '../../apis/instance.js';
+import { checkOwner } from "/src/utils/checkUser.js";
+import { fetchComments, deleteComment } from "/src/apis/postApi.js";
+
+import useInfiniteList from '/src/hooks/userInfiniteList.js';
 import "./comment.css";
 
 export default function Comment({ postId, onDeleteComment }) {
-  const [comments, setComments] = useState([]);
-  const [page, setPage] = useState(0);
-  const [hasNext, setHasNext] = useState(true);
-  const observerRef = useRef(null);
 
-  const reqGetComments = async () => {
+  const handleDeleteComment = (commentInfo) => {
     try {
-      if (!hasNext) return;
+      if (!commentInfo.commentId)  throw new Error("commentId is NULL");
 
-      if (!postId) {
-        console.warn("postId is not provided to the Comment component.");
+      if (!checkOwner(commentInfo.userId)) {
+        alert("삭제 권한이 없습니다.")
         return;
       }
 
-      const res = await axiosInstance.get(`http://localhost:8080/comment/list`, {
-        params: { postId, page: page, size: 10 },
-      });
+      deleteComment(commentInfo.commentId);
 
-      const data = res.data;
-
-      console.log(data);
-
-      setComments(prev => [...prev, ...data.content]);
-      setHasNext(!data.isLast);
-      setPage(prev => prev + 1);
-    } catch (error) {
-      console.log(error);      
-    }
-  };
-
-  const reqDeleteComment = async (commentId) => {
-    try {
-      const token = localStorage.getItem('token');
-
-      if (!token) throw new Exception();
-
-      const res = await axiosInstance.delete(`http://localhost:8080/comment/${commentId}`,
-        {
-          headers: {
-            Authorization: `Bearer ${token}`
-          }
-        }
-      );
-
-    } catch (error) {
-      console.log(error);
-      alert('댓글 삭제에 실패했습니다. 다시 시도해주세요.');
-    }
-  }
-
-  const deleteComment = (commentInfo) => {
-    if (!checkOwner(commentInfo.userId)) {
-      alert("삭제할 권한이 없습니다.")
-      return;
-    }
-
-    reqDeleteComment(commentInfo.commentId);
-
-    if (onDeleteComment) {
-      onDeleteComment();
-    }
-
-    setComments(prev => prev.filter(comment => comment.commentId !== commentInfo.commentId));
-  }
-
-  // 무한 스크롤 Intersection Observer 설정
-  useEffect(() => {
-    const observer = new IntersectionObserver(
-      entries => {
-        if (entries[0].isIntersecting && hasNext) {
-          reqGetComments();
-        }
-      },
-      { threshold: 1 }
-    );
-
-    if (observerRef.current) observer.observe(observerRef.current);
-    return () => observer.disconnect();
-  }, [comments, hasNext]);
-
-  const getLoggedInUserId = () => {
-    const token = localStorage.getItem('token');
-    if (token) {
-      try {
-        const decodedToken = jwtDecode(token);
-        return decodedToken.sub;
-      } catch (error) {
-        console.error('Decoding Error : ', error);
-        return null;
+      if (onDeleteComment) {
+        onDeleteComment();
       }
-    }
-  };
 
-  const checkOwner = (authorId) => {
-    const loggedInUserId = getLoggedInUserId();
-    return loggedInUserId && loggedInUserId === authorId;
-  };
+      setComments(prev => prev.filter(comment => comment.commentId !== commentInfo.commentId));
+    } catch (error) {
+      console.error('Delete Comment Error : ', error); 
+    }
+  }
+
+  const commentsFetcher = useCallback(async (offset, limit) => {
+    return fetchComments(postId, offset, limit);
+  }, [postId]);
+
+  const {
+    items: comments,
+    hasMore,
+    loading,
+    lastRef
+  } = useInfiniteList(commentsFetcher, 10);
 
   return (
     <>
       <div className="comment_list">
         {
           comments.map((comment, index) => {
+            const isLastComment = comments.length === index + 1; // for useInfiniteList
             return (
-              <div className="comment_item" key={index}>
+              <div className="comment_item" 
+                key={index}
+                ref={isLastComment ? lastRef : null} // for useInfiniteList
+              >
                 <div className="comment_meta">
                   <img className="user_avatar" src={`/src/assets/images/profile/profile_${comment.profileImg}.png`} alt="User Avatar" />
                   <span className="comment_author">{comment.nickname}</span>
                   {
                     checkOwner(comment.userId) ? (
                       <button className="delete_comment_button" aria-label="Delete comment"
-                        onClick={() => deleteComment(comment)}>
+                        onClick={() => handleDeleteComment(comment)}>
                         <img src="/src/assets/icons/trash.png" alt="Delete" className="delete_icon" />
                       </button>
                     ) : null
@@ -128,9 +69,7 @@ export default function Comment({ postId, onDeleteComment }) {
             )
           })
         }
-        {hasNext && (
-          <div ref={observerRef} style={{ height: '10px', background: 'transparent' }}></div>
-        )}
+        {loading && <p>댓글 로딩 중...</p>}
       </div>
     </>
   );
