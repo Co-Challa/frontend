6d82fbf847ed49884e5a24f8519eb30c8d5dfb4e
dahee <ldhdh1832@gmail.com>
2025-05-21 21:48:53 +0900

    image 수정

--- src/components/MainPost.jsx
+++ src/components/MainPost.jsx
@@ -2,12 +2,7 @@ import React from "react";
 
 import "./mainPage.css";
 import { useNavigate } from "react-router-dom";
-import profile1 from "../assets/images/profile/profile_1.png";
-import profile2 from "../assets/images/profile/profile_2.png";
-import profile3 from "../assets/images/profile/profile_3.png";
-import profile4 from "../assets/images/profile/profile_4.png";
-import profile5 from "../assets/images/profile/profile_5.png";
-import profile6 from "../assets/images/profile/profile_6.png";
+
 
 export default function MainPost({ post }) {
   const navigate = useNavigate();
@@ -32,16 +27,15 @@ export default function MainPost({ post }) {
   };
   console.log(post);
 
-  const profileMap = {
-    1: profile1,
-    2: profile2,
-    3: profile3,
-    4: profile4,
-    5: profile5,
-    6: profile6
-  };
+  const profileImages = import.meta.glob('../assets/images/profile/*.png', {
+    eager: true,
+    import: 'default'
+  });
 
-  const imgSrc = profileMap[profileImgCode];
+  const imgSrc = profileImages[`../assets/images/profile/profile_${profileImgCode}.png`];
+
+
+
 
   return (
     <div
@@ -73,21 +67,16 @@ export default function MainPost({ post }) {
       <div className="post_contnet">{content}</div>
 
       {/* 좋아요와 댓글 */}
-      <div className="interaction_bar">
-        {/*좋아요 버튼 */}
-        <div className="icon_group">
-          {/* <div className={`like_button ${liked ? "liked" : "unliked"}`}></div> */}
-          <span>{likesCount}</span>
-        </div>
 
-        {/* 댓글 아이콘 */}
-        <div className="icon_group">
-          <div className="comments_icon"></div>
-          <span>{commentsCount}</span>
-        </div>
+
+
+      {/* 댓글 아이콘 */}
+      <div className="icon_group">
+        <div className="comments_icon"></div>
+        <span>{commentsCount}</span>
       </div>
-
-
     </div>
+
+
   );
 }
