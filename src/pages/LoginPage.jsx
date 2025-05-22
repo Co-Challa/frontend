df01332b6f2ba1d3ab728fdd9c0c712aea198f8c
seoyun <chaeseoyoun8694@gmail.com>
2025-05-22 17:17:00 +0900

    feat. 403 에러/토큰 없을때 로그인 페이지 이동
    +) 로그인 후 로그인 직전 페이지로 이동

--- src/pages/LoginPage.jsx
+++ src/pages/LoginPage.jsx
@@ -1,40 +1,37 @@
 import axios from "axios";
 import { useEffect, useState } from "react";
-import { useNavigate } from "react-router-dom";
+import { useNavigate, useSearchParams, Link } from "react-router-dom";
 import "./LoginPage.css";
 
 export default function LoginPage() {
-
   const navigate = useNavigate();
+  const [searchParams] = useSearchParams();
+  // 쿼리에서 from을 꺼내고 디코딩
+  const rawFrom = searchParams.get("from");
+  const from = rawFrom ? decodeURIComponent(rawFrom) : "/";
 
-  const [form, setForm] = useState({
-    userId: "",
-    password: "",
-  });
+  const [form, setForm] = useState({ userId: "", password: "" });
 
   useEffect(() => {
     const token = localStorage.getItem("token");
     if (token) {
-      alert("이미 로그인 한 상태입니다.");
-      navigate("/");
+      alert("이미 로그인 상태입니다.");
+      navigate(from, { replace: true });
     }
-  }, []);
+  }, [from, navigate]);
 
-  const handleChange = (e) => {
-    setForm({ ...form, [e.target.name]: e.target.value });
-  }
+  const handleChange = (e) =>
+    setForm(prev => ({ ...prev, [e.target.name]: e.target.value }));
 
   const handleSubmit = async (e) => {
     e.preventDefault();
     try {
-      const response = await axios.post("http://localhost:8080/signin", form);
-      const token = response.headers["authorization"]?.split(" ")[1];
-      console.log(response)
-
+      const res = await axios.post("http://localhost:8080/signin", form);
+      const token = res.headers.authorization?.split(" ")[1];
       if (token) {
         localStorage.setItem("token", token);
         alert("로그인 성공!");
-        navigate("/");
+        navigate(from, { replace: true });
       } else {
         alert("토큰이 없습니다.");
       }
@@ -42,20 +39,34 @@ export default function LoginPage() {
       const msg = err.response?.data ?? "로그인 실패";
       alert("에러: " + msg);
     }
-  }
+  };
+  
   return (
     <div className="login-page">
       <div className="login-container">
         <form className="login-form" onSubmit={handleSubmit}>
           <h2>로그인</h2>
           <label htmlFor="userId">아이디</label>
-          <input name="userId" placeholder="아이디를 입력해주세요" value={form.userId} onChange={handleChange} required />
+          <input
+            name="userId"
+            placeholder="아이디를 입력해주세요"
+            value={form.userId}
+            onChange={handleChange}
+            required
+          />
           <label htmlFor="password">비밀번호</label>
-          <input name="password" type="password" placeholder="비밀번호를 입력해주세요" value={form.password} onChange={handleChange} required />
+          <input
+            name="password"
+            type="password"
+            placeholder="비밀번호를 입력해주세요"
+            value={form.password}
+            onChange={handleChange}
+            required
+          />
           <button type="submit">로그인</button>
           <div className="links">
             <span>계정이 없으신가요?</span>
-            <a href="/signup">회원가입</a>
+            <Link to="/signup">회원가입</Link>
           </div>
         </form>
       </div>
@@ -63,6 +74,5 @@ export default function LoginPage() {
         <img src="/src/assets/images/bg_chatbot.png" alt="일러스트" />
       </div>
     </div>
-
   );
 }
